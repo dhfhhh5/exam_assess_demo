87de76b6f1ac95ca43fb118786d0ec2555bc9de1
dhfhhh5 <2646484616@qq.com>
2024-09-29 10:31:31 +0800

    Initial commit

---
+++ src/main/java/com/exam/exam_assess_demo/ExamAssessDemoApplication.java
@@ -0,0 +1,13 @@
+package com.exam.exam_assess_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamAssessDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamAssessDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/exam_assess_demo/ExamAssessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.exam_assess_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAssessDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
